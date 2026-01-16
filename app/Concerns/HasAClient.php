d2018017664c5d10aaf983721080f93fb1651232
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2026-01-15 22:08:30 -0500

    Update HasAClient.php

--- app/Concerns/HasAClient.php
+++ app/Concerns/HasAClient.php
@@ -13,24 +13,32 @@ trait HasAClient
 
     protected function ensureClient()
     {
-        $config = app(ConfigRepository::class);
-
-        $apiKey = $config->get('api_key');
-
-        if (! $apiKey) {
-            info('No API key found!');
-            info('Learn how to generate a key: https://cloud.laravel.com/docs/api/authentication#create-an-api-token');
-
-            $apiKey = password(
-                label: 'Laravel Cloud API key',
-                required: true,
-            );
-
-            $config->set('api_key', $apiKey);
-
-            info('API key saved to '.$config->path());
-        }
+        $apiKey = $this->resolveApiKey();
 
         $this->client = new CloudClient($apiKey);
+    }
+
+    protected function resolveApiKey(): string
+    {
+        $config = app(ConfigRepository::class);
+        $apiKey = $config->get('api_key');
+
+        if ($apiKey) {
+            return $apiKey;
+        }
+
+        info('No API key found!');
+        info('Learn how to generate a key: https://cloud.laravel.com/docs/api/authentication#create-an-api-token');
+
+        $apiKey = password(
+            label: 'Laravel Cloud API key',
+            required: true,
+        );
+
+        $config->set('api_key', $apiKey);
+
+        info('API key saved to '.$config->path());
+
+        return $apiKey;
     }
 }
